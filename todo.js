6971fcf513a881199ac574f3730cb7ee9e10ac04
elliotegere1-hue <elliotegere1@gmail.com>
2026-07-19 12:53:11 +0100

    uses loops

--- todo.js
+++ todo.js
@@ -1,7 +1,7 @@
 let toDoList=[
     { day: "Monday", todo: []},
     { day: "Tuesday", todo: []},
-    { day: "Wednsday", todo: []},
+    { day: "Wednesday", todo: []},
     { day: "Thursday", todo: []},
     { day: "Friday", todo: []},
     { day: "Saturday", todo: []},
@@ -10,63 +10,49 @@ let toDoList=[
 
 ]
 
-let count=0
+   let count=0
+
+
 
 function addTask(){
     let task=document.getElementById("TaskName").value
     let day=document.getElementById("day").value
     let priority=document.getElementById("selectBox").value
     let notes=document.getElementById("notes").value
-    let current=0
+
     
 
     let taskString=`Task: ${task} \n
     priority: ${priority} \n
     notes: ${notes}`
 
-    if (day== "Monday"){
-    toDoList[0].todo.push(taskString)
-    current=0
-    count=count+1
-    }else if(day=="Tuesday"){
-     toDoList[1].todo.push(taskString)
-    current=1
-    count+=1
-    } else if(day=="Wednsday"){
-     toDoList[2].todo.push(taskString)
-    current=2
-    count+=1
-    }else if(day=="Thursday"){
-     toDoList[3].todo.push(taskString)
-    current=3
-    count+=1
-    }else if(day=="Friday"){
-     toDoList[4].todo.push(taskString)
-    current=4
-    count+=1
-    }else if(day=="Saturday"){
-     toDoList[5].todo.push(taskString)
-    current=5
-    count+=1
-    }else if(day=="Sunday"){
-     toDoList[6].todo.push(taskString)
-    current=6
-    count+=1
-    }
 
-    document.getElementById("count").textContent=count 
+for (const key in toDoList) { // the code loops through the keys in the object
+  if (toDoList[key].day === day) { // it then says if the to do list keys day in that iteration is the same as day that we decalred above 
+    toDoList[key].todo.push(taskString) // push task string into the current itterations key todolist
+    count++
+  }
+}
+
+    document.getElementById("count").textContent = count // ADDED: update count on page
+
+
+
 
     
     let list=document.getElementById("ul")
     
-    list.innerHTML+=
-    `<li>${toDoList[current].day} - ${toDoList[current].todo}</li>`
+   list.innerHTML = "" // wipe the list clean first
+
+for (let i = 0; i < toDoList.length; i++) {
+  if (toDoList[i].todo.length > 0) {
+    list.innerHTML += `<li>${toDoList[i].day} - ${toDoList[i].todo}</li>`
+  }
+}
     
        
     
-    if (toDoList[current].todo.length){
-    toDoList[current].todo.shift()
-    }
+    
 
 document.getElementById("TaskName").value=""
     document.getElementById("day").value=""
